a05d6201e255100af84a276f3eaf5f9a939d54af
Omkar Patil <omkarpatil@Omkars-MacBook-Pro.local>
2019-07-14 18:41:53 +0530

    Added initial code

---
+++ Game.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	//var refree = map[string]interface{}{}
+	var playerarr = []string{"Joey", "Monica", "Chandler", "Ross", "Phoebe", "Rachel", "Sachin", "Rohan"}
+	var players = map[string]int{}
+	players["Joey"] = 7
+	players["Monica"] = 6
+	players["Chandler"] = 6
+	players["Ross"] = 5
+	players["Phoebe"] = 5
+	players["Rachel"] = 6
+	players["Sachin"] = 4
+	players["Rohan"] = 5
+
+	var finalscorecard = map[string]map[string]int{}
+
+	fmt.Printf("Player array : %v \n", players)
+	fmt.Printf("Player names : %v \n", playerarr)
+	shuffledarr := shuffle(playerarr)
+	fmt.Printf("Random array  :%v \n", shuffledarr)
+	finalscorecard["Round1"] = gameplay(shuffledarr, players)
+	//fmt.Printf("Final Score Card : %v \n", finalscorecard)
+	shuffledarr = getwinners(finalscorecard["Round1"])
+	finalscorecard["Round2"] = gameplay(shuffledarr, players)
+	//fmt.Printf("Final Score Card R2: %v \n", finalscorecard)
+	shuffledarr = getwinners(finalscorecard["Round2"])
+	finalscorecard["Round3"] = gameplay(shuffledarr, players)
+	fmt.Printf("Final Score Card: %v \n", finalscorecard)
+	shuffledarr = getwinners(finalscorecard["Round3"])
+	fmt.Printf("Champion ===> %v \n\n", shuffledarr)
+	finalscores, err := json.Marshal(finalscorecard)
+	if err != nil {
+		fmt.Printf("JSON Encoding Failed")
+		fmt.Printf("Final Score Card: %v \n", finalscorecard)
+	}
+	fmt.Printf("Final Score Card: %v \n", string(finalscores))
+}
+func getwinners(scorearr map[string]int) []string {
+	var winnerarr = []string{}
+	for k, v := range scorearr {
+		if v == 5 {
+			winnerarr = append(winnerarr, k)
+		}
+	}
+	winners := shuffle(winnerarr)
+	return winners
+
+}
+func gameplay(shuffledarr []string, players map[string]int) map[string]int {
+	var scorecard = map[string]int{}
+	for k := 0; k < len(shuffledarr); {
+		play(shuffledarr[k:k+2], players, scorecard)
+		k = k + 2
+	}
+	fmt.Printf("SCORECARD :%v \n", scorecard)
+	return scorecard
+
+}
+func play(playerrr []string, players map[string]int, scorecard map[string]int) {
+	offensive := playerrr[0]
+	defender := playerrr[1]
+	fmt.Printf("Offensive : %v Defender : %v \n ", offensive, defender)
+	num := getrandomnumber()
+	defensivearr := getrandomarray(players[defender])
+	fmt.Printf("Generated number : %v  Defensive array : %v \n", num, defensivearr)
+	exist := checknumberexist(num, defensivearr)
+	if exist == 0 {
+		//fmt.Println("Offensive is Winner!!!")
+		scorecard[offensive] = scorecard[offensive] + 1
+		if scorecard[offensive] < 5 {
+			play(playerrr, players, scorecard)
+		} else if scorecard[offensive] == 5 {
+			fmt.Println(offensive + " is Winner! of Game *****************!!")
+			return
+		}
+
+	} else {
+		//fmt.Println("Defender is Winner!!!")
+		scorecard[defender] = scorecard[defender] + 1
+		if scorecard[defender] < 5 {
+			playerswitch := []string{defender, offensive}
+			play(playerswitch, players, scorecard)
+		} else if scorecard[defender] == 5 {
+			fmt.Println(defender + " is Winner! of Game ********************!!")
+			return
+		}
+	}
+	// playerswitch := []string{defender, offensive}
+	// play(playerswitch, players)
+	//fmt.Printf("SCORECARD :%v \n", scorecard)
+}
+
+func shuffle(src []string) []string {
+	final := make([]string, len(src))
+	rand.Seed(time.Now().UTC().UnixNano())
+	perm := rand.Perm(len(src))
+
+	for i, v := range perm {
+		final[v] = src[i]
+	}
+	return final
+}
+
+func getrandomnumber() int {
+	rand.Seed(time.Now().UTC().UnixNano())
+	num := rand.Intn(10)
+	return num
+
+}
+
+func getrandomarray(size int) []int {
+	rand.Seed(time.Now().UTC().UnixNano())
+	perm := rand.Perm(size)
+	return perm
+
+}
+
+func checknumberexist(num int, defensivearr []int) int {
+	for _, v := range defensivearr {
+		if num == v {
+			return 1
+		}
+	}
+	return 0
+}
